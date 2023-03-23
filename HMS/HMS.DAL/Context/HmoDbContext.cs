9037ba8898ef5c0593e9b5a570eb25dc7be44c2d
Kaybangz <igKaybangz@gmail.com>
2023-03-23 15:59:27 +0100

    Added Validation to Entities

--- HMS/HMS.DAL/Context/HmoDbContext.cs
+++ HMS/HMS.DAL/Context/HmoDbContext.cs
@@ -11,26 +11,78 @@ namespace HMS.DAL.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            //
+            base.OnModelCreating(modelBuilder);
 
-            
+
+
             modelBuilder.Entity<AppUser>(e =>
             {
                 e.Property(u => u.Email)
                 .HasMaxLength(50)
-                .IsRequired();
-                e.HasIndex(u => u.Email)
+                .IsRequired()
+                .HasAnnotation("ErrorMessage", "Email field is required");
+                e.HasIndex(u => u.Email, "IX_UniqueEmail")
                  .IsUnique();
             });
-
-
 
             modelBuilder.Entity<AppUser>()
                 .Property(u => u.UserName)
                 .HasMaxLength(100)
-                .IsRequired();
+                .IsRequired()
+                .HasAnnotation("ErrorMessage", "Username field is required");
 
-            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<AppUser>()
+                .Property(u => u.FirstName)
+                .HasMaxLength(100)
+                .IsRequired()
+                .HasAnnotation("ErrorMessage", "Firstname field is required");
+
+            modelBuilder.Entity<AppUser>()
+                .Property(u => u.LastName)
+                .HasMaxLength(100)
+                .IsRequired()
+                .HasAnnotation("ErrorMessage", "Lastname field is required");
+
+            modelBuilder.Entity<AppUser>()
+                .Property(u => u.MiddleName)
+                .HasMaxLength(100)
+                .IsRequired(false);
+
+            modelBuilder.Entity<Appointment>()
+                .Property(a => a.Reason)
+                .HasMaxLength(1000)
+                .IsRequired()
+                .HasAnnotation("ErrorMessage", "Reason field is required");
+
+            modelBuilder.Entity<Plan>()
+                .Property(p => p.Name)
+                .HasMaxLength(100)
+                .IsRequired()
+                .HasAnnotation("ErrorMessage", "Name field is required");
+
+            modelBuilder.Entity<Plan>()
+                .Property(p => p.Price)
+                .HasPrecision(15, 2)
+                .IsRequired()
+                .HasAnnotation("ErrorMessage", "Price field is required");
+
+            modelBuilder.Entity<Drug>()
+                .Property(d => d.Name)
+                .HasMaxLength(100)
+                .IsRequired()
+                .HasAnnotation("ErrorMessage", "Name field is required");
+
+            modelBuilder.Entity<Drug>()
+                .Property(d => d.Description)
+                .HasMaxLength(1000)
+                .IsRequired(false);
+
+
+            modelBuilder.Entity<Drug>()
+                .Property(d => d.Price)
+                .HasPrecision(15, 2)
+                .IsRequired()
+                .HasAnnotation("ErrorMessage", "Price field is required");
         }
     }
 }
--- HMS/HMS.DAL/Entities/Plan.cs
+++ HMS/HMS.DAL/Entities/Plan.cs
@@ -4,10 +4,13 @@ using HMS.DAL.Enums;
 namespace HMS.DAL.Entities
 {
     public class Plan
-    { 
+    {
         public int Id { get; set; }
         public string Name { get; set; }
         public PlanType PlanType { get; set; }
-        public decimal Price { get; set; }  
+        public decimal Price { get; set; }
+
+        public AppUser AppUser { get; set; }
+        public Drug Drug { get; set; }
     }
 }
