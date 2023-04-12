1209d0c406b36ccd97762f6dc2d44923203cc85d
Kaybangz <igKaybangz@gmail.com>
2023-04-12 10:41:58 +0100

    Added actions to enrollee controller

--- HMS/HMS.DAL/Dtos/Requests/EnrolleeDto.cs
+++ HMS/HMS.DAL/Dtos/Requests/EnrolleeDto.cs
@@ -1,11 +1,25 @@
-﻿using HMS.DAL.Entities;
+﻿using HMS.DAL.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HMS.DAL.Dtos.Requests
 {
-    public class EnrolleeDTO
+    public class EnrolleeDto
     {
-        public int Id { get; set; }
+        [Required(ErrorMessage = "First name is required"), MaxLength(100)]
+        public string FirstName { get; set; } = null!;
+
+        [Required(ErrorMessage = "Last name is required"), MaxLength(100)]
+        public string LastName { get; set; } = null!;
+
+        [MaxLength(100)]
+        public string? MiddleName { get; set; }
+        public string? Address { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+
+
+        [ForeignKey("Plan")]
         public int? PlanId { get; set; }
-        public Plan? Plan { get; set; }
     }
 }
